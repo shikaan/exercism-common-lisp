4c8430eedce9e33e493de989277bd7f4a390f8f6
Manuel Spagnolo <spagnolo.manu@gmail.com>
2022-12-27 00:20:00 +0100

    feat: hamming

--- hamming/hamming.lisp
+++ hamming/hamming.lisp
@@ -7,4 +7,4 @@
 (defun distance (dna1 dna2)
   "Number of positional differences in two equal length dna strands."
   (if (= (length dna1) (length dna2))
-    (length (remove-if-not #'null (map 'list #'char= dna1 dna2)))))
+    (count nil (map 'list #'char= dna1 dna2))))
