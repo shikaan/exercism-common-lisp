2b465afb0fb591c0121b6352330a1cd37394bfa9
Manuel Spagnolo <spagnolo.manu@gmail.com>
2023-01-02 22:10:38 +0100

    feat: all-your-base

--- all-your-base/all-your-base.lisp
+++ all-your-base/all-your-base.lisp
@@ -4,15 +4,13 @@
 
 (in-package :all-your-base)
 
-(defun rebase (list-digits in-base out-base)
-  (convert-decimal-to (to-decimal list-digits in-base) out-base))
-
 (defun to-decimal (list-digits in-base)
   "Converts a number given as a list of digits and a base to decimal"
-  (loop 
-    for digit in (reverse list-digits)
-    for i from 0 
-    sum (* digit (expt in-base i))))
+  (if (zerop (length list-digits)) 0
+    (loop 
+      for digit in (reverse list-digits)
+      for i from 0 
+      sum (* digit (expt in-base i)))))
 
 (defun div (num den) 
   "Performs a division with remainder"
@@ -21,6 +19,18 @@
 (defun convert-decimal-to (n base &optional (acc '()))
   "Converts a decimal to a given base"
   (if (zerop n) 
-    (reverse acc)
+    (if (zerop (length acc)) '(0) (reverse acc))
     (multiple-value-bind (q r) (div n base) 
       (convert-decimal-to q base (append acc (list r))))))
+
+(defun valid-p (digits base) 
+  (every #'(lambda (x) (and (<= 0 x) (< x base)))  digits))
+
+(defun rebase (list-digits in-base out-base)
+  (cond
+    ((< in-base 2) nil)
+    ((< out-base 2) nil)
+    ((null list-digits) '(0))
+    ((not (valid-p list-digits in-base)) nil)
+    (t (convert-decimal-to (to-decimal list-digits in-base) out-base))
+  ))
