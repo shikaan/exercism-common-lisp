e6fb0e936e1d0fd4059f20a9d94ae72c3c86d824
Manuel Spagnolo <spagnolo.manu@gmail.com>
2023-01-15 22:48:20 +0100

    feat: diamond

--- diamond/diamond.lisp
+++ diamond/diamond.lisp
@@ -21,14 +21,14 @@
   "Returns an empty string of length n"
   (coerce (loop for i from 1 to n collect #\Space) 'string))
 
-(defun up-to-and-down-to (n) 
-  "Return a list (0...n...0)"
-  (append (loop for i from 0 to n collect i) (loop for i from (1- n) downto 0 collect i)))
-
 (defun rows (letter)
-  (let* ((idx (position letter *alphabet*)) (size (diamond-size idx)) (half-size (floor (/ size 2))))
-    (loop 
-      for row in (up-to-and-down-to idx)
+  (let* (
+    (idx (position letter *alphabet*)) 
+    (size (diamond-size idx)) 
+    (half-size (floor (/ size 2)))
+    (half-diamond (loop 
+      for row from 0 to idx
       for l-idx = (mod row (1+ idx))
       for l = (char *alphabet* l-idx)
       collect (insert-at (- half-size l-idx) l (insert-at (+ half-size l-idx) l (make-line size))))))
+    (append half-diamond (reverse (butlast half-diamond)))))
