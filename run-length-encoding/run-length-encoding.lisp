f69f9f81a043f33f2ac812bf4b51e0392e2a28a7
Manuel Spagnolo <spagnolo.manu@gmail.com>
2023-01-17 22:45:57 +0100

    feat: run-length-encoding

--- run-length-encoding/run-length-encoding.lisp
+++ run-length-encoding/run-length-encoding.lisp
@@ -9,27 +9,28 @@
   "Returns t if a sequence is empty"
   (zerop (length seq)))
 
-(defun split-if (test seq &optional accumulator)
+(defun concat (s1 s2) (format nil "~a~a" s1 s2))
+
+(defun split-if (test str)
   "Splits a sequence on every item satisfying the test"
-  (if (empty-p seq)
-      (reverse accumulator)
-      (let ((chunk-end (position-if test seq)) (subs nil) (total-length (length seq)))
-        (if (null chunk-end) (setf chunk-end total-length))
-        (setf subs (subseq seq (min (1+ chunk-end) total-length)))
-        (let ((chunk (subseq seq 0 (1+ chunk-end))))
-          (unless (empty-p chunk) (push chunk accumulator))
-          (split-if test subs accumulator)))))
+  (loop
+    for el across str with result with subs = ""
+    if (not (funcall test el)) 
+      do (setf subs (concat subs el))
+    else 
+      do (progn (push (concat subs el) result) (setf subs ""))
+    finally 
+      (progn (unless (empty-p subs) (push subs result)) (return (reverse result)))))
 
 (defun group-by (test str)
   "Groups chars in a string based on a test"
   (reverse
     (reduce
-        #'(lambda (acc c)
-            (let ((most-recent (first acc)))
-              (let ((group (first most-recent)))
-                (if (or (null group) (funcall test c group))
-                    (cons (push c most-recent) (rest acc))
-                    (push (list c) acc)))))
+      #'(lambda (result element)
+          (let* ((most-recent (first result)) (most-recent-element (first most-recent)))
+            (if (or (null most-recent-element) (funcall test element most-recent-element))
+              (cons (push element most-recent) (rest result))
+              (push (list element) result))))
       str
       :initial-value '())))
 
@@ -37,7 +38,7 @@
   "Makes a sequence of type (x, ...x) -> Nx"
   (if (= (length l) 1)
       (format nil "~a" (first l))
-      (format nil "~a~a" (length l) (first l))))
+      (concat (length l) (first l))))
 
 (defun decode-symbol (s)
   "Decode a symbol of type Nx into a string xxx...x"
@@ -45,23 +46,16 @@
     (let ((c (char s (1- size))) (times (if (= 1 size) 1 (parse-integer (subseq s 0 (1- size))))))
       (coerce (loop for i from 1 to times collect c) 'string))))
 
-
 (defun encode (plain)
-  (if (empty-p plain)
-      ""
-      (reduce 
-          #'(lambda (acc l)
-              (format nil "~a~a" acc (encode-sequence l)))
-        (group-by #'char= plain)
-        :initial-value "")))
+  (reduce #'concat
+    (group-by #'char= plain)
+    :key #'encode-sequence
+    :initial-value ""))
 
 (defun allowed-char (x) (or (alpha-char-p x) (char= x #\Space)))
 
 (defun decode (compressed)
-  (if (empty-p compressed)
-      ""
-      (reduce
-          #'(lambda (acc s)
-              (format nil "~a~a" acc (decode-symbol s)))
-        (split-if #'allowed-char compressed) 
-        :initial-value "")))
+  (reduce #'concat
+    (split-if #'allowed-char compressed)
+    :key #'decode-symbol
+    :initial-value ""))
