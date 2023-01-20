8ef1ad5ab7f6767b1c74b5d595dc8f171ca05005
Manuel Spagnolo <spagnolo.manu@gmail.com>
2023-01-20 22:05:42 +0100

    feat: darts

--- darts/darts.lisp
+++ darts/darts.lisp
@@ -5,9 +5,9 @@
 (in-package :darts)
 
 (defun score (x y)
-  (let ((r (sqrt (+ (* x x) (* y y))))
+  (let ((r (sqrt (+ (* x x) (* y y)))))
     (cond
-     ((and (> r 5) (< r 10)) 1)
+     ((and (> r 5) (<= r 10)) 1)
      ((and (> r 1) (<= r 5)) 5)
-     ((< r 1) 10)
-     (t 0)))))
+     ((<= r 1) 10)
+     (t 0))))
