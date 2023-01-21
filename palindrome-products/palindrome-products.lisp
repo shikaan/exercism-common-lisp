83111179d8e643da7424df577aecfa968cdd81fe
Manuel Spagnolo <spagnolo.manu@gmail.com>
2023-01-22 00:03:14 +0100

    feat: palindrome-products

--- palindrome-products/palindrome-products.lisp
+++ palindrome-products/palindrome-products.lisp
@@ -9,30 +9,26 @@
   (let ((s (format nil "~a" n)))
     (string= s (reverse s))))
 
+(defun palindrome-between (min max)
+  (loop for i from min to max if (palindrome-p i) collect i))
+
+(defun palindrome-between-2 (min max)
+  (loop for i from max downto min if (palindrome-p i) collect i))
+
+(defun get-factors (n minimum maximum)
+  (loop with result for a from minimum to (min n maximum)
+    do (loop for b from a to (min n maximum)
+      if (= (* a b) n) do (push (list a b) result))
+    finally (return (reverse result))))
+
 (defun smallest (min-factor max-factor)
   (if (< min-factor max-factor)
-    (loop 
-      with minimum = nil
-      with minimum-couple = '() 
-      for a from min-factor to max-factor
-        do (loop for b from a to max-factor
-            for p = (* a b)
-            if (palindrome-p p)
-            do (cond
-              ((or (null minimum) (< p minimum)) (progn (setf minimum p) (setf minimum-couple (list `(,a ,b)))))
-              ((= p minimum) (setf minimum-couple (append minimum-couple (list `(,a ,b)))))))
-        finally (return (values minimum minimum-couple)))))
+    (let ((palindromes (palindrome-between (expt min-factor 2) (expt max-factor 2))))
+      (loop for i in palindromes for factors = (get-factors i min-factor max-factor) 
+        if factors do (return (values i factors))))))
 
 (defun largest (min-factor max-factor)
   (if (< min-factor max-factor)
-    (loop 
-      with maximum = nil
-      with maximum-couple = '() 
-      for a from min-factor to max-factor
-        do (loop for b from a to max-factor
-            for p = (* a b) 
-            if (palindrome-p p)
-            do (cond
-              ((or (null maximum) (> p maximum)) (progn (setf maximum p) (setf maximum-couple (list `(,a ,b)))))
-              ((= p maximum) (setf maximum-couple (append maximum-couple (list `(,a ,b)))))))
-        finally (return (values maximum maximum-couple)))))
+    (let ((palindromes (palindrome-between-2 (expt min-factor 2) (expt max-factor 2))))
+      (loop for i in palindromes for factors = (get-factors i min-factor max-factor)
+        if factors do (return (values i factors))))))
