b0ad6cdd259bd4bfd408454e6b12d76a978aec6b
Manuel Spagnolo <spagnolo.manu@gmail.com>
2023-01-21 18:28:39 +0100

    feat: palindrome-products

---
+++ palindrome-products/palindrome-products.lisp
@@ -0,0 +1,49 @@
+(defpackage :palindrome-products
+  (:use :cl)
+  (:export :smallest
+           :largest))
+
+(in-package :palindrome-products)
+
+(defun palindrome-p (n)
+  (let* ((s (format nil "~a" n)) (l (length s)))
+    (if (= 1 l) t
+      (loop for i from 0 to (ceiling (/ l 2))  
+        always (char= (char s i) (char s (- (1- l) i)))))))
+
+
+(defun couples (min-factor max-factor)
+  (loop 
+    with result
+    for a from min-factor to max-factor 
+      do (loop for b from min-factor to max-factor 
+        do (push (list a b) result))
+    finally (return (reverse result))))
+
+(defun smallest (min-factor max-factor)
+  (if (< min-factor max-factor)
+    (loop 
+      with minimum = nil
+      with minimum-couple = '() 
+      for a from min-factor to max-factor
+        do (loop for b from a to max-factor
+            for p = (* a b)
+            if (palindrome-p p)
+            do (cond
+              ((or (null minimum) (< p minimum)) (progn (setf minimum p) (setf minimum-couple (list `(,a ,b)))))
+              ((= p minimum) (setf minimum-couple (append minimum-couple (list `(,a ,b)))))))
+        finally (return (values minimum minimum-couple)))))
+
+(defun largest (min-factor max-factor)
+  (if (< min-factor max-factor)
+    (loop 
+      with maximum = nil
+      with maximum-couple = '() 
+      for a from min-factor to max-factor
+        do (loop for b from a to max-factor
+            for p = (* a b) 
+            if (palindrome-p p)
+            do (cond
+              ((or (null maximum) (> p maximum)) (progn (setf maximum p) (setf maximum-couple (list `(,a ,b)))))
+              ((= p maximum) (setf maximum-couple (append maximum-couple (list `(,a ,b)))))))
+        finally (return (values maximum maximum-couple)))))
