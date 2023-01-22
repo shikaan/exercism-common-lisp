8dbe7e728dec5ac3464f1b2e0baf0c6d2df6be39
Manuel Spagnolo <spagnolo.manu@gmail.com>
2023-01-22 23:57:11 +0100

    feat: rail-fence-cipher

--- rail-fence-cipher/rail-fence-cipher.lisp
+++ rail-fence-cipher/rail-fence-cipher.lisp
@@ -7,42 +7,30 @@
 
 (defun clean-input (msg) (remove #\Space msg))
 
-(defun nth-even (n) (* 2 n))
+(defun circular! (items)
+  "Modifies the last cdr of list ITEMS, returning a circular list"
+  (setf (cdr (last items)) items))
 
-(defun skip-pattern (rail rails)
-  (cond 
-    ((or (zerop rail) (= rail (1- rails))) (list (nth-even (1- rails))))
-    (t (list (nth-even (- rails (1+ rail))) (nth-even rail)))))
+(defun up-down (n cap) 
+  (let* (
+    (b (append (loop for i from 1 to n collect i) (loop for i from (1- n) downto 2 collect i)))
+    (base (circular! b)))
+    (loop for i from 0 to cap collect (nth i base))))
 
-(defun collect-chars-at-rail (str rail rails)
-  (let ((pattern (skip-pattern rail rails)))
-  (loop for i from rail below (length str)
-        for j from 0
-        for c = (char str i)
-        with next = 0
-        if (= next j)
-        collect c and do 
-          (progn 
-            (setf next (+ next (first pattern)))
-            (setf pattern (reverse pattern))))))
+(defun encode-list (l rails)
+  (mapcar #'first
+      (sort 
+        (mapcar #'list 
+          l 
+          (up-down rails (length l))) #'< :key #'second)))
 
 (defun encode (msg rails)
   (setf msg (clean-input msg))
-  (loop for r from 0 below rails with result
-    do (setf result (append result (collect-chars-at-rail msg r rails)))
-    finally (return (coerce result 'string))))
-
-(defun insert-at (str c pos) (setf (aref str pos) c))
+  (coerce (encode-list (coerce msg 'list) rails) 'string))
 
 (defun decode (msg rails)
-  (let* ((result (make-string (length msg))) (base (nth-even (1- rails))))
-
-  ))
-
-; f(0, 4) = 0
-; f(1, 4) = 6       pattern-1
-; f(2, 4) = 12      2x pattern-1
-; f(3, 4) = 18      3x pattern-1
-; f(4, 4) = 1       rail + pattern-1
-; f(5, 4) = 5
-; f(6, 4) = 7
+  (coerce (mapcar #'first
+    (sort 
+      (mapcar #'list 
+        (coerce msg 'list) 
+        (encode-list (loop for i from 1 to (length msg) collect i) rails)) #'< :key #'second)) 'string))
