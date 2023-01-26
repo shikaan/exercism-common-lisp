ad5a2a82f2e2ed3d9d2213f2a65ec11a06dfccea
Manuel Spagnolo <spagnolo.manu@gmail.com>
2023-01-26 23:02:28 +0100

    feat: book-store

--- book-store/book-store.lisp
+++ book-store/book-store.lisp
@@ -13,7 +13,7 @@
 (defun groups-of (l n &optional accumulator)
   (let* (
     (fst (first l))
-    (matched (remove-if-not (equal-n-p fst) (rest l))))
+    (matched (remove-if-not (equal-n-p fst) (remove-duplicates (rest l)))))
     (setf matched (push fst matched))
     (if (>= (length matched) n)
       (let* (
@@ -22,18 +22,31 @@
         (groups-of remainder n (push sublist accumulator)))
       (list accumulator l))))
 
-(defun price (l)
-  (cond ((not (listp l)) 800)
-        ((= (length l) 5) 3000)
-        ((= (length l) 4) 2560)
-        ((= (length l) 3) 2160)
-        ((= (length l) 2) 1520)
-        (t 800)))
+; In order to optimise groups we need to replace the (5,3) couples
+; with (4,4) couples. These ocuples will include placeholders,
+; since the actual books don't matter for price calc.
+(defun opt (bins)
+  (loop while (and (> (first bins) 0) (> (third bins) 0))
+    do (setf bins 
+        (list 
+          (1- (first bins))
+          (+ 2 (second bins))
+          (1- (third bins))
+          (fourth bins)
+          (fifth bins))))
+  bins)
 
 (defun calculate-price (basket)
   (let* (
     (five (groups-of basket 5))
     (four (groups-of (second five) 4))
     (three (groups-of (second four) 3))
-    (two (groups-of (second three) 2)))  
-    (reduce #'+ (append (first five) (first four) (first three) (first two) (second two)) :key #'price)))
+    (two (groups-of (second three) 2))
+    (bins (list (length (first five)) (length (first four)) (length (first three)) (length (first two)) (length (second two)))))
+    (setf bins (opt bins))
+    (+
+      (* 3000 (first bins))
+      (* 2560 (second bins))
+      (* 2160 (third bins))
+      (* 1520 (fourth bins))
+      (* 800 (fifth bins)))))
